4ef09e62b7d9ec416b5c5bbd5d1869c495e29099
Andrew Tauber <atauber@vmware.com>
2022-08-03 14:34:07 -0400

    rebuild code

--- tools/kubeconfig-builder-job/deploy/deploy.sh
+++ tools/kubeconfig-builder-job/deploy/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Build and Push Image for the job
-docker build --platform amd64  -t kubeconfig-builder-job:1.0 -f Dockerfile .
-docker tag kubeconfig-builder-job:1.0 us-west1-docker.pkg.dev/prow-tkg-build/prow-prod-registry/kubeconfig-builder-job:1.0
-docker push us-west1-docker.pkg.dev/prow-tkg-build/prow-prod-registry/kubeconfig-builder-job:1.0
+docker build --platform amd64  -t kubeconfig-builder-job:3.0 -f Dockerfile .
+docker tag kubeconfig-builder-job:3.0 us-west1-docker.pkg.dev/prow-open-btr/prow-sandbox-registry/kubeconfig-builder-job:3.0
+docker push us-west1-docker.pkg.dev/prow-open-btr/prow-sandbox-registry/kubeconfig-builder-job:3.0
 
 # Create the CR, CRD and service account for the cluster authentication in the default namespace
 kubectl apply -f cluster/clusterrole.yaml
 
-# Create the artifact registry secrets in the namespace to acces images from 
+# Create the artifact registry secrets in the namespace to acces images from
 kubectl create secret docker-registry artifact-registry \
     --docker-server=https://us-west1-docker.pkg.dev \
     --docker-email=711165799992-compute@developer.gserviceaccount.com \
@@ -22,4 +22,3 @@ kubectl create secret generic secret-manager-credentials --from-file=serviceacco
 
 # Deploy the configmap and the cronjob
 kubectl apply -f cluster/cronjob.yaml
-
